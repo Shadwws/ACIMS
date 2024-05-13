c47f99dec8354f15ecd5da79bcb1dea6d57121f9
Sandy <134401016+TheSandyy@users.noreply.github.com>
2024-05-13 02:40:47 -0300

    Arreglado el punto c

--- punto_c.asm
+++ punto_c.asm
@@ -1,214 +1,90 @@
-#Encontrar la posición del valor máximo en el arreglo .
-.data 
-	arreglo: .word 57, 102, 209, 64, 30, 147, 122, 215, 33, 78, 183, 97, 138, 62, 175, 40, 93, 117, 21, 9
-	contador: .word 0
-	tamaño_arreglo: .word 20
-	arreglo_ordenado: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	print_resultado: .asciiz "El valor maximo en el  arreglo es: "
-	print_posicion: .asciiz "La posicicion del valor maximo es: "
-	print_arreglo: .asciiz "El arreglo utilizado es el siguiente: "
-	nueva_linea: .asciiz "\n"
-	coma: .asciiz ", "
-	abrir_corchete: .asciiz "["
-	cerrar_corchete: .asciiz "]"
-	
-	abc: .asciiz "El arreglo ordenado queda: "
-	
-	ordenar: .asciiz "ordenado es "
-		
+.data
+arreglo: .word 12, 255, 200, 150, 50, 33, 133, 100, 17, 15, 33, 188, 0, 10, 12, 40, 201, 7, 9, 10
+tamano: .word 8
+espacio: .asciiz " "
+nueva_linea: .asciiz "\n"
+
 .text
-	la $s0, arreglo_ordenado
-	la $t1, arreglo
-	lw $s7, arreglo
-	lw $t2, tamaño_arreglo
-	li $t8, 19
-	li $t9, 0
-	
-	
-	mayor:
-			
-			encontrar:
-			#busca en la array
-				bge $s2, $t2, guardar #Si el contador es igual o mayor al tamaño del arreglo detengo el bucle
-				lw $s5, ($t1)                                  
-				ble $s5, $s1, aumento #Si el valor de A[contador] es menor aumenta sin cambiar
-			
-			cambiar:
-			#si es mayor cambia el temporal
-				move $s1, $s5 #Muevo el A[contador] a la variable donde está el elemento mas grande.
-				move $t4, $s2 #guardo la posición del índice actual menos uno.
-			
-			aumento:
-			#aumenta el contador y avanza en la array
-				addi $t1, $t1, 4
-				addi $s2, $s2, 1 #Sumo uno al contador para seguir iterando, es como en un while.
-			
-			
-				j encontrar #Vuelvo al bucle.
-			
-			
-			guardar:
-			#encontrado se guarda en la 2da array y avanzo
-				sw $s1, 0($s0) 	
-				
-				li $v0, 4
-				la $a0, ordenar
-				syscall
-				
-				li $v0, 1
-				lw $a0, 0($s0) 
-				syscall	
-				
-				li $v0, 4
-				la $a0, nueva_linea
-				syscall
-				
-				li $v0, 4
-				la $a0, nueva_linea
-				syscall
-							
-				addi $s0, $s0, 4			
-				
-				
-			mostrar:
-			#muestro el valr mas grande actual
-				li $v0, 4
-				la $a0, print_resultado
-				syscall
-			
-				li $v0, 1
-				move $a0, $s1 
-				syscall
-			
-				li $v0, 4
-				la $a0, nueva_linea
-				syscall
-			
-				li $v0, 4
-				la $a0, print_posicion
-				syscall
-			
-				li $v0, 1
-				move $a0, $t4
-				syscall
-			
-				li $v0, 4
-				la $a0, nueva_linea
-				syscall
-				
-				
-			
-			borrar:
-			#borro el elemento mayor de la 1ra array
-				subi $t1, $t1, 80
-				mul $t4, $t4, 4
-				add $t1, $t1, $t4
-				sw $zero, ($t1)
-				lw $s5, ($t1)
-				sub $t1, $t1, $t4
-				
-			mostrar_arreglo:	
-					
-				
-				
-				# Imprimir el primer corchete
-				li $v0, 4
-				la $a0, abrir_corchete
-				syscall
+.globl main
 
-				# Iterar sobre el arreglo
-				li $t3, 0  # Reiniciar el contador de posición del arreglo
-				li $t4, 0  # Reiniciar el flag para imprimir coma
-				li $t5, 4  # Tamaño de una palabra en bytes
+main:
+    la $s7, arreglo       # Cargar la dirección base del arreglo
+    lw $s6, tamano        # Cargar el tamaño del arreglo
+    li $t0, 0             # Inicializar el índice del bucle externo
+    mul $t9, $s6, 4       # Calcular el desplazamiento máximo del arreglo
+    j Bubblesort
 
-			iterar_arreglo:
-				# Calcular la dirección del elemento actual del arreglo
-				mul $t6, $t3, $t5
-				add $t7, $t1, $t6
+Bubblesort:
+    li $s1, 0            # Inicializar contador interno
+    li $t0, 0            # Inicializar índice externo
+    
+bucle_externo:
+    beq $t0, $s6, fin    # Si el índice externo es igual al tamaño, terminar
+    
+    li $t1, 0            # Inicializar el índice del bucle interno
+    li $t2, 0            # Inicializar el flag de intercambio
+    
+bucle_interno:
+    mul $t3, $t1, 4      # Calcular el desplazamiento
+    add $t4, $s7, $t3    # Calcular la dirección del elemento actual
+    lw $t5, ($t4)        # Cargar el elemento actual
+    lw $t6, 4($t4)       # Cargar el siguiente elemento
+    
+    blt $t6, $t5, no_swap   # Si el elemento actual es menor o igual que el siguiente, no intercambiar
+    jal swap               # Intercambiar los elementos
+    li $t2, 1              # Establecer el flag de intercambio a 1
+    
+no_swap:
+    addi $t1, $t1, 1     # Incrementar el índice del bucle interno
+    blt $t1, $s6, bucle_interno   # Si no hemos llegado al final del arreglo, continuar el bucle interno
+    
+    beqz $t2, siguiente_externo  # Si no se realizó ningún intercambio en este paso, continuar con el siguiente índice externo
+    addi $s1, $s1, 1     # Incrementar el contador de iteraciones
+    blt $s1, $s6, bucle_externo   # Si no hemos completado todas las iteraciones, continuar con el bucle externo
 
-				# Cargar el elemento actual del arreglo
-				lw $a0, ($t7)
-				li $v0, 1
-				syscall
+siguiente_externo:
+    addi $t0, $t0, 1     # Incrementar el índice del bucle externo
+    j bucle_externo
 
-				# Imprimir coma si no es el último elemento
-				addi $t3, $t3, 1
-				bne $t3, $t2, imprimir_coma
+fin:
+    j mostrar_arreglo     # Mostrar el arreglo
 
-				# Imprimir el último corchete y saltar al finalizar
-				li $v0, 4
-				la $a0, cerrar_corchete
-				syscall
-				
-				li $v0, 4
-				la $a0, nueva_linea
-				syscall
-				
-				j reinicio
+swap:
+    lw $t7, ($t4)        # Guardar el valor del primer elemento
+    lw $t8, 4($t4)       # Guardar el valor del segundo elemento
+    sw $t8, 0($t4)        # Colocar el valor del segundo elemento en la posición del primero
+    sw $t7, 4($t4)       # Colocar el valor del primer elemento en la posición del segundo
+    jr $ra               # Retornar
 
-			imprimir_coma:
-				li $v0, 4
-				la $a0, coma
-				syscall
+mostrar_arreglo:
+    li $v0, 4            # Imprimir cadena de espacio
+    la $a0, espacio
+    syscall
 
-				j iterar_arreglo
-	
-			
-				
-				
-				
-			
-	reinicio:
-		#agrego al contador y reinicio el bucle
-			li $s2, 0
-			li $t4, 0
-			add $t9, $t9, 1
-			move $s1, $s5
-			ble $t9, $t8, mayor	
-	
-	mostrar_ordenado:
-	
-		li $t3, 0
-		li $t4, 0 
-		li $t5, 4
-		subi $s0, $s0, 80
-		
-		li $v0, 4
-		la $a0, abc
-		syscall
-		
-		li $v0, 4
-		la $a0, nueva_linea
-		syscall
-		
-		ordenado:
-			mul $t6, $t3, $t5
-			add $t7, $s0, $t6
-			
-			lw $a0, ($t7)
-			li $v0, 1
-			syscall
-		
-		  	addi $t3, $t3, 1
-			bne $t3, $t2, comaas
-			
-			li $v0, 4
-			la $a0, cerrar_corchete
-			syscall
-			
-			j fin_mostrar_arreglo
-		
-		comaas:
-			li $v0, 4
-			la $a0, coma
-			syscall
+    li $t0, 0            # Inicializar el índice del arreglo
 
-			j ordenado
-		
-	
-		fin_mostrar_arreglo:
-   		li $v0, 10
-   		syscall #Fin del programa
+loop_imprimir:
+    mul $t1, $t0, 4      # Calcular el desplazamiento
+    add $t2, $s7, $t1    # Calcular la dirección del elemento i
+    lw $a0, ($t2)        # Cargar el elemento i
+    li $v0, 1            # Imprimir el elemento
+    syscall
+    
+    li $v0, 4            # Imprimir cadena de espacio
+    la $a0, espacio
+    syscall
+    
+    addi $t0, $t0, 1     # Incrementar el índice i
+    blt $t0, $s6, loop_imprimir
 
+    la $a0, nueva_linea  # Imprimir nueva línea
+    li $v0, 4
+    syscall
+    
+    j fin_mostrar_arreglo  # Finalizar el programa
+
+fin_mostrar_arreglo:
+    li $v0, 10
+    syscall               # Fin del programa
 
 
