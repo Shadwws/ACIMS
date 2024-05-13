e32c35fe9868dff559f1b4efa1a9bfeff30ea920
Sandy <134401016+TheSandyy@users.noreply.github.com>
2024-05-13 04:36:20 -0300

    Terminada la documentación del factorial

--- factorial_recursivo.asm
+++ factorial_recursivo.asm
@@ -4,9 +4,12 @@
 	
 .text
 	.global main:
-		lw $a0, buscado
+
+		lw $a0, buscado #Se carga el factorial que se desea encontrar.
 		
 		jal factorial
+
+		# Para este punto, el resultado ya está almacenado en $v0
 	
 		sw $v0, resultado
 		
@@ -18,25 +21,25 @@
 		syscall
 	
 	factorial:
-		addi $sp, $sp, -8
-		sw $ra, 0($sp)
-		sw $s0, 4($sp)
+		addi $sp, $sp, -8 # Se reserva memoria en el stack para los casos recursivos.
+		sw $ra, 0($sp) # Se guarda la referencia del programa y también los resultados parciales.
+		sw $s0, 4($sp) 
 	
 		#caso base
 		
-		li $v0, 1
-		beq $a0, 0, fin
+		li $v0, 1  
+		beq $a0, 0, fin # Si la entrada es igual a cero termino la recursión
 		
-		move $s0, $a0
-		subi $a0, $a0, 1
+		move $s0, $a0 # Si no, copio el valor de la entrada a $s0.
+		subi $a0, $a0, 1 #decremento uno a la entrada para lograr el n - 1
 		jal factorial
 		
-		mul $v0, $s0, $v0
+		mul $v0, $s0, $v0 #Aca es donde se hace la multiplicación de n * n- 1 y se calcula el factorial.
 	
 	fin:
 		lw $ra, ($sp)
 		lw $s0, 4($sp)
-		addu $sp, $sp, 8
+		addu $sp, $sp, 8 #Devuelvo la memoria reservada en el stack y regreso al main con el resultado en $v0.
 		
 		jr $ra
 		
